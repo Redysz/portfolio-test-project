d7aefa66cff42e3b249307e85b10f6b36cb4ea1a
Redysz <tuliokkj@gmail.com>
2020-03-22 14:44:41 +0100

    Update admin site

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Job
+from .models import Job, Translation, Configuration
 
 admin.site.register(Job)
+admin.site.register(Translation)
+admin.site.register(Configuration)
