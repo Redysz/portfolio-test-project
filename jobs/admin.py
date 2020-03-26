2712be930ddf4671309c6475a0ccd939384ff853
Redysz <tuliokkj@gmail.com>
2020-03-26 12:40:49 +0100

    Skills

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Job, Translation, Configuration
+from .models import Job, Translation, Configuration, Skill
 
 admin.site.register(Job)
 admin.site.register(Translation)
 admin.site.register(Configuration)
+admin.site.register(Skill)
--- jobs/views.py
+++ jobs/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
-from .models import Job
+from .models import Job, Skill
 from blog.traslation_manager import translator, global_translations, reload_global_translations_with_language
 from blog.utils import get_lang_from_request
 
 def home(request):
     jobs = Job.objects.all().order_by('-id')[:4]
+    skills = Skill.objects.all()
     translations = dict()
     reload_global_translations_with_language(get_lang_from_request(request))
     translations['Hello'] = translator.get_translation('TR_HELLO')
     translations['Welcome'] = translator.get_translation('TR_WELCOME')
     translations['Write'] = translator.get_translation('TR_WRITE_TO_ME')
     translations.update(global_translations)
-    return render(request, 'jobs/home.html', {'jobs':jobs, 'translations': translations})
+    return render(request, 'jobs/home.html', {'jobs': jobs, 'skills': skills, 'translations': translations})
 
 def donate(request):
     translations = dict()
